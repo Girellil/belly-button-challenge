d10948cb885266a7741990acb317a4f9b45c6a0a
Girellil <64373104+Girellil@users.noreply.github.com>
2024-02-15 18:26:18 -0500

    Challenge Solution
    Final Push

--- README.md
+++ README.md
@@ -1,46 +1,21 @@
 # belly-button-challenge
 
-Solution for the proposal of the project 2 (Option 1 - Default) presented.
+Solution for the Cahllenge of Module 14 ( Belly Button Biodiversity) presented.
 
 
 ## The solution for this Project is presented in the following structure:
 
-1) Crowdfunding_ETL/README.md (this file).
+1) belly-button-challenge/README.md (this file).
 
-2) Crowdfunding_ETL/Resources/ = Folder containing the resource files in .XLSX format along with the .CSV output files:
-    - /contacts.xlsx = Original source table of contacts.
-    - /crowdfunding.xlsx = Original source table of the campaign info.
-    - /campaign.csv = Output campaign table with the transformed Data.
-    - /category.csv = Output categories table.
-    - /subcategory.csv = Output sub-categories table.
-    - /contacts.csv = Output contacts table with the transformed Data.
+2) belly-button-challenge/index.html = Starter HTML File containing structure of WebApp page.
 
-3) Crowdfunding_ETL/ETL_Mini_Project_Starter_Code.ipynb = Notebook file used to:
-    - Load the source .xlsx files,
-    - Inspect and transformt the data,
-    - Create and configure Pandas DataFrames with the new data to be used in the new DB,
-    - Outputting the Dataframes into .CSV files.
+3) belly-button-challenge/samples.json = json File containing a sample of the dataset to be imported and analysed.
 
-4) Crowdfunding_ETL/resources_scan.ipynb = Notebook file used to inspect the file names and columns of the output files to be loaded into the new DB.
+4) belly-button-challenge/static/
+    - app.js = File containing the JavaScript code ran in the app.
 
-5) Crowdfunding_ETL/Crowfunding_DB/ = Folder containing the files involved in the creation and execution of the Database:
-    - /crowdfunding_db_BU.sql = Backup file of the output Database.
-    - /crowdfunding_db_schema.sql = Schema output from QuickDB, used to query the creation of Database Tables, formats and relatioships in .TXT format.
-    - /QuickDBD-export.png = Output of schema image from QuickDB.
-        ![campaign](/Crowfunding_DB/QuickDBD-export.png)
-    - /QuickDBD-export.txt = Prompt input in QuickDB to obtain schema image and sql-query from QuickDB.
-    - /Select-[Campaign, Cat, Subcat, Contacts].png = Screenshots of "SELECT * from {Tablename}" queries to test result of DB load process.
-
-    ![campaign](/Crowfunding_DB/Select-Campaign.png)
-    ---
-    ![cat](/Crowfunding_DB/Select-Cat.png)
-    ---
-    ![subcat](/Crowfunding_DB/Select-Subcat.png)
-    ---
-    ![contacts](/Crowfunding_DB/Select-Contacts.png)
-
-    QuickDB ('https://app.quickdatabasediagrams.com/#/')
-
+Dashboard format - initial Page:
+    ![Dashboard](/starter-screen.png)
 
 
 
--- static/js/app.js
+++ static/js/app.js
@@ -11,65 +11,130 @@ function getJson(url){
 
 //Initial function to add Data names to dropdown Menu
 function init(data){
+  //get the list of names from the data
   let names = data.names;
-
+  //select the menu to add the names from the data imported
   let subjectDropDown = d3.select("#selDataset");
   
+  //loop through Names adding all to the list in the dropdown menu
   for(let i = 0; i < names.length; i++){
     subjectDropDown.append("option").text(names[i]);
   }
-}
 
-//Initialize Json and starter Function
-getJson(dataURL).then(init);
+  // Call option changed function to start dashboard with fisrt item on the names
+  optionChanged(names[0]);
+
+}
 
 // get selection cahnge from HTML
 function optionChanged(selectedID){
+  //call getJson Function and then statement with function to update the 3 graphs
   getJson(dataURL).then(function(data){
+    //get samples and and metadata lists from dataset
     let sampleList = data.samples;
     let metaData = data.metadata;
 
-    for(let i = 0; i < sampleList.length; i++){
-      if(sampleList[i].id == selectedID){
-        let subjectOTUList = sampleList[i].otu_ids.slice(0,10);
-        let subjectOTUCount = sampleList[i].sample_values.slice(0,10);
-        let subjectOTULabels = sampleList[i].otu_labels.slice(0,10);
+    // Loop through sample to find ID
+    for(let looper1 = 0; looper1 < sampleList.length; looper1++){
+      if(sampleList[looper1].id == selectedID){
 
-        yValues = subjectOTUList.map(num => "OTU " + num);
+        //start list of original values from sample
+        let OTUList  = sampleList[looper1].otu_ids
+        let OTUCounts = sampleList[looper1].sample_values
+        let OTUlabels = sampleList[looper1].otu_labels
 
-        let trace1 = [{
-           x : subjectOTUCount.reverse(),
+        //Sort list based on counts and follow lists of names and labels based on indexes
+        let sortedOTUCounts = OTUCounts.sort((a, b) => b - a);
+        let sortedOTUList = OTUList.slice().sort((a, b) => OTUList[OTUCounts.indexOf(b)] - OTUList[OTUCounts.indexOf(a)]);
+        let sortedOTUlabels = OTUlabels.slice().sort((a, b) => OTUlabels[OTUCounts.indexOf(b)] - OTUlabels[OTUCounts.indexOf(a)]);
+
+        //start buble graphics info from ORIGINAL list
+        let traceBubble = [{
+          x : OTUList,
+          y : OTUCounts,
+          text : OTUlabels,
+          mode: 'markers',
+          hoverinfo : "text",
+            marker: {
+                size: OTUCounts,
+                color: OTUList,
+            },
+          type : "scatter",
+        }];
+        //Start bubble graphic layout
+        let layoutBubble = {
+          title : "OTU Count Scatter",
+          xaxis: {title : "OTU ID"},
+          yaxis: {title : "OTU Quantity"}
+        };
+        //Plot bubble graph into bubble id.
+        Plotly.newPlot("bubble", traceBubble, layoutBubble);
+
+        //start sliced lists for bar Graph from SORTED lists
+        let topOTUList = sortedOTUList.slice(0,10);
+        let topOTUCounts = sortedOTUCounts.slice(0,10);
+        let topOTULabels = sortedOTUlabels.slice(0,10);
+
+        //Add prefix to OTU id list
+        yValues = topOTUList.map(num => "OTU " + num);
+
+        //start bar graph info
+        let traceBar = [{
+           x : topOTUCounts.reverse(),
            y : yValues.reverse(),
-           text : subjectOTULabels.reverse(),
+           text : topOTULabels.reverse(),
            type : "bar",
            orientation : "h",
            hoverinfo : "text"
          }];
-         
-         let layout = {
+         //start bargraph layout
+         let layoutBar = {
           yaxis: {
             tickvals: yValues.reverse(), 
             ticktext: yValues.reverse()
             }
           };
-
-          Plotly.newPlot("plot1", trace1, layout);
+          //plot bargraph into bar id.
+          Plotly.newPlot("bar", traceBar, layoutBar);
+          //break to stop loop since id found
+          break;
 
       }
     }
+    // start loop into MetaData list to extract id's MetaData info
+    for(let looper2 = 0 ; looper2 < metaData.length; looper2++){
+      if(metaData[looper2].id == selectedID){
+        //initialize variables for each metaData feature along with description
+        let thisID = "id: " + selectedID;
+        let ethnicity = "ethnicity:" + metaData[looper2].ethnicity;
+        let gender = "gender: " + metaData[looper2].gender;
+        let age = "age: " + metaData[looper2].age;
+        let location = "location: " + metaData[looper2].location;
+        let bbtype = "bbtype: " + metaData[looper2].bbtype;
+        let wfreq = "wfreq: " + metaData[looper2].wfreq;
+        
+        //Add MetaData to List
+        let metaDataInfo = [thisID, ethnicity, gender, age, location, bbtype, wfreq]
 
-    for(let j = 0 ; j < metaData.length; j++){
-      if(metaData[j].id == selectedID){
-        let ethnicity = metaData[j].ethnicity;
-        let gender = metaData[j].gender;
-        let age = metaData[j].age;
-        let location = metaData[j].location;
-        let bbtype = metaData[j].bbtype;
-        let wfreq = metaData[j].wfreq;
-        let chosenMetaData = [ethnicity, gender, age, location, bbtype, wfreq]
-        console.log(chosenMetaData)
+        //Select the metaData Box
+        let metaDataPanel = d3.select("#sample-metadata")
+
+        //Clear MetaData box from previous values
+        metaDataPanel.selectAll("*").remove();
+
+        //Loop through list to add each element into a new line in the box
+        for(let k = 0; k < metaDataInfo.length; k++){
+          metaDataPanel.append("p").text(metaDataInfo[k])
+        }
+        
+        //break to stop loop since id found
+        break;
+        
       }
-    }
+    }    
     
   });
 }
+
+//Initialize Json and starter Function
+getJson(dataURL).then(init);
